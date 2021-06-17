c1a1c66b45e9040a9c506c50a522760f40439b26
ernestkz <ernestkuznecovs@gmail.com>
2021-06-17 23:14:05 +0100

    adts.

---
+++ src/Generator.hs
@@ -0,0 +1,11 @@
+module Generator where
+
+import           System.Random
+
+
+-- pluggable for different data types
+-- configurable to change the event occurances
+
+-- a generic config where you can change the occurances of each field in the datatypes
+
+-- can just leave this out and come back later when know more about generics when merging event streams with different data types
--- src/RedTeam.hs
+++ src/RedTeam.hs
@@ -1,3 +1,12 @@
 module RedTeam where
 
-blah = undefined
+data ButtonClick = ButtonClick User ButtonType
+  deriving Show
+
+newtype User = User String
+  deriving Show
+
+data ButtonType = ButtonA | ButtonB | ButtonC
+  deriving Show
+
+
